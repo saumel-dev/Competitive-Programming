bd2c9bfb6470e7dd5692ba9edc0957327f60b4b8
Saumel-Das <saumel.spr@gmail.com>
2026-06-14 21:14:03 +0600

    solved

---
+++ C++ Basic/G_Factorial.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a,b) memset(a, b, sizeof(a))
+#define check cout << "-__________________-" << endl
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    ll res = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        res *= i; 
+    }
+    
+    cout << res << endl;
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
